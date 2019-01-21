74c49db682f6ea5ff361b5767a1e6b40df3b594a
codematrix <ross_pellegrino@hotmail.com>
2019-01-20 22:05:46 -0500

    Adding Plato.Serializer

--- src/Plato.Redis/Interfaces/IRedisCollectionFactory.cs
+++ src/Plato.Redis/Interfaces/IRedisCollectionFactory.cs
@@ -1,4 +1,8 @@
-﻿using Plato.Redis.Interfaces;
+﻿// Plato.Core
+// Copyright (c) 2019 ReflectSoftware Inc.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
+
+using Plato.Redis.Interfaces;
 using StackExchange.Redis;
 
 namespace Plato.Redis.Interfaces
