a2ca5f1affa3ce82d45468137ac6a38dbc24f718
codematrix <ross_pellegrino@hotmail.com>
2019-02-06 18:26:23 -0500

    Applying header

--- src/Plato/Miscellaneous/LogTextFileWriterFactory.cs
+++ src/Plato/Miscellaneous/LogTextFileWriterFactory.cs
@@ -1,4 +1,8 @@
-﻿using Plato.Interfaces;
+﻿// Plato.Core
+// Copyright (c) 2019 ReflectSoftware Inc.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
+
+using Plato.Interfaces;
 
 namespace Plato.Miscellaneous
 {
